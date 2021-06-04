1e3c914f7f008acd5d4ef4e7aa400f1ce5bfc126
ErinCleaver-GRCC <erincleaver@email.grcc.edu>
2021-06-03 20:28:22 -0400

    1 file modified

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 
 
-const printChar = (char, start, rows) => {
+const printChar = (start, rows) => {
   if(start > rows) {
     return "";
   } else if(rows == 1) {
     return char;
   }
+  let char1 = "*";
+  let char2 = "#"
 
-  console.log(`%c ${char.repeat(start)}`, 'color: lightblue')
-  return printChar(char, start + 1, rows)
+  console.log(`%c ${char1.repeat(start)}`, 'color: lightblue')
+  return printChar(start + 1, rows)
+
+  console.log(`%c ${char2.repeat(start)}`, 'color: lightblue')
+  return printChar(start + 1, rows)
 }
-printChar("*", 1, 6)
-printChar("#", 1, 6)
+printChar(1, 6)
